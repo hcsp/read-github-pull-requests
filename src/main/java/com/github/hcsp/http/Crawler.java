b1dd45a8a5537e081046c0b3229105df70d50467
huangjiaxi <huangjiaxi@oak-inc.com>
2019-07-31 14:07:50 +0800

    Reformat Code

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -32,26 +32,27 @@ public class Crawler {
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
     public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
-        String url = "https://github.com/"+repo+"/pulls";
-        String html = Crawler.getUrlContent(url,"UTF-8");
+        String url = "https://github.com/" + repo + "/pulls";
+        String html = Crawler.getUrlContent(url, "UTF-8");
         Document doc = Jsoup.parse(html);
         Elements issueRow = doc.select(".js-navigation-container .js-navigation-item.js-issue-row");
         List<GitHubPullRequest> result = new ArrayList<>();
         for (Element row : issueRow) {
-            int number = Integer.parseInt(row.select(".js-navigation-open").attr("id").replace("issue_","").replace("_link",""));
+            int number = Integer.parseInt(row.select(".js-navigation-open").attr("id").replace("issue_", "").replace("_link", ""));
             String title = row.select(".js-navigation-open").text();
             String author = row.select(".opened-by .muted-link").text();
-            result.add(new GitHubPullRequest(number,title,author));
+            result.add(new GitHubPullRequest(number, title, author));
         }
         return result;
     }
 
-    private static String getUrlContent(String url,String charset) throws IOException {
+    private static String getUrlContent(String url, String charset) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(url);
         CloseableHttpResponse response1 = httpclient.execute(httpGet);
-        return EntityUtils.toString(response1.getEntity(),charset);
+        return EntityUtils.toString(response1.getEntity(), charset);
     }
+
     public static void main(String[] args) throws IOException {
         getFirstPageOfPullRequests("gradle/gradle");
     }
