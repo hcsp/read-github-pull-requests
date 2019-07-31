7f5e388ccc2805336303984a699249b68e69d11f
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-31 17:48:45 +0800

    Jsoup (#9)
    Jsoup (#9)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,12 @@
 package com.github.hcsp.http;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +26,19 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        ArrayList<GitHubPullRequest> response = new ArrayList<>();
+        String url = "https://github.com/"+ repo + "/pulls";
+        Document doc = Jsoup.connect(url).get();
+        Elements elements = doc.select(".js-issue-row");
+        for (Element e : elements){
+            String[] number_temp = e.child(0).child(1).select(".text-small").text().split(" ");
+            int number = Integer.valueOf(number_temp[0].substring(1));
+            String title = e.child(0).child(1).child(0).text();
+            String author = e.child(0).child(1).select(".muted-link").first().text();
+            response.add(new GitHubPullRequest(number, title, author));
+        }
+        return response;
+    }
+
 }
