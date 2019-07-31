552f02be2cf82b22d492fb30a38fd74f821ad8ed
zyw <40329182+zhangyongwang4444@users.noreply.github.com>
2019-07-31 19:38:37 +0800

    使用github api 获取gradle/gradle首页的 pull request (#6)
    使用github api 获取gradle/gradle首页的 pull request (#6)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,8 +1,21 @@
 package com.github.hcsp.http;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import com.alibaba.fastjson.JSON;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Crawler {
+    public static void main(String[] args) throws IOException {
+        getFirstPageOfPullRequests("gradle/gradle");
+    }
+
     static class GitHubPullRequest {
         // Pull request的编号
         int number;
@@ -18,6 +31,70 @@ public class Crawler {
         }
     }
 
+    static class GitHubListPullRequestResponse {
+        int number;
+        String title;
+        User user;
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+
+        static class User {
+            String login;
+
+            public String getLogin() {
+                return login;
+            }
+
+            public void setLogin(String login) {
+                this.login = login;
+            }
+        }
+    }
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+
+        // see https://developer.github.com/v3/pulls/#list-pull-requests
+        final String RepoURL = "https://api.github.com/repos/" + repo + "/pulls";
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(RepoURL);
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        String responseJson = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+
+        List<GitHubListPullRequestResponse> gitHubListPullRequestResponses = JSON.parseArray(responseJson, GitHubListPullRequestResponse.class);
+
+
+        return gitHubListPullRequestResponses
+                .stream()
+                .map(x -> new GitHubPullRequest(x.getNumber(), x.getTitle(), x.getUser().getLogin()))
+                .collect(Collectors.toList());
+    }
+
 }
