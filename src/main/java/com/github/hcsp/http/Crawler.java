ffdb334cccef497259f5a91401bd4573367f40a9
catsme <845058547@qq.com>
2019-08-01 10:08:45 +0800

    修复了bug

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -30,30 +30,6 @@ public class Crawler {
         // Pull request的作者的GitHub id
         String author;
 
-        public int getNumber() {
-            return number;
-        }
-
-        public void setNumber(int number) {
-            this.number = number;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-
         GitHubPullRequest(int number, String title, String author) {
             this.number = number;
             this.title = title;
@@ -64,14 +40,13 @@ public class Crawler {
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
     public static ArrayList<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
         ArrayList<GitHubPullRequest> gitHubPullRequestIfo = new ArrayList<>();
-        GitHubPullRequest newGitHubPullRequest = new GitHubPullRequest(0, "null", "null");
         CloseableHttpClient aDefaultHttpClient = HttpClients.createDefault();
         HttpGet getAUrl = new HttpGet("https://github.com/" + repo + "/pulls");
         CloseableHttpResponse getResponseFromUrl = aDefaultHttpClient.execute(getAUrl);
         HttpEntity aHttpEntityFromUrl = getResponseFromUrl.getEntity();
         if (aHttpEntityFromUrl != null) {
             try {
-                gitHubPullRequestIfo = getUsefulMessage(newGitHubPullRequest, aHttpEntityFromUrl);
+                gitHubPullRequestIfo = getUsefulMessage(aHttpEntityFromUrl);
             } finally {
                 getResponseFromUrl.close();
             }
@@ -79,7 +54,7 @@ public class Crawler {
         return gitHubPullRequestIfo;
     }
 
-    private static ArrayList<GitHubPullRequest> getUsefulMessage(GitHubPullRequest newGitHubPullRequest, HttpEntity aHttpEntityFromUrl) throws IOException {
+    private static ArrayList<GitHubPullRequest> getUsefulMessage(HttpEntity aHttpEntityFromUrl) throws IOException {
         ArrayList<GitHubPullRequest> arr = new ArrayList<>();
         InputStream inputStream = aHttpEntityFromUrl.getContent();
         String html = IOUtils.toString(inputStream, "utf-8");
@@ -87,11 +62,12 @@ public class Crawler {
         ArrayList<Element> elementArrayList = document.select(".js-issue-row");
         for (Element element : elementArrayList
         ) {
-            newGitHubPullRequest.setTitle(element.selectFirst("a").text());
-            newGitHubPullRequest.setNumber(Integer.parseInt(element.selectFirst("a").attr("id").replaceAll("[^\\d]", "")));
+            String title = element.selectFirst("a").text();
+            int number = Integer.parseInt(element.selectFirst("a").attr("id").replaceAll("[^\\d]", ""));
             String[] aStrFromSplitElement = element.selectFirst("span.opened-by").text().split(" ");
-            newGitHubPullRequest.setAuthor(aStrFromSplitElement[aStrFromSplitElement.length - 1]);
-            arr.add(newGitHubPullRequest);
+            String author = (aStrFromSplitElement[aStrFromSplitElement.length - 1]);
+            GitHubPullRequest aTempGitHubPullRequest = new GitHubPullRequest(number, title, author);
+            arr.add(aTempGitHubPullRequest);
         }
         return arr;
     }
