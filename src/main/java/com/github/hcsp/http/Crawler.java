bd911924302b8a7b4e011b98947d3bfd9fa6f547
Moon <justwe77@163.com>
2019-08-11 22:43:22 +0800

    modify

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,8 +1,35 @@
 package com.github.hcsp.http;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class Crawler {
+    // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        Document doc = Jsoup.connect("https://github.com/" + repo + "/pulls").get();
+        Element issues = doc.getElementsByAttributeValue("aria-label", "Issues").get(1).child(0);
+
+        List<GitHubPullRequest> gitHubPullRequestList = new ArrayList<>();
+
+        for (Element issue : issues.children()) {
+            int number = Integer.parseInt(issue.id().replace("issue_", ""));
+            String title = issue.child(0).child(1).child(0).text();
+            String author = issue.child(0).child(1).getElementsByClass("text-small").get(0)
+                    .getElementsByClass("opened-by")
+                    .get(0).getElementsByTag("a").get(0).text();
+            GitHubPullRequest gitHubPullRequest = new GitHubPullRequest(number, title, author);
+            gitHubPullRequestList.add(gitHubPullRequest);
+        }
+
+        return gitHubPullRequestList;
+    }
+
     static class GitHubPullRequest {
         // Pull request的编号
         int number;
@@ -17,7 +44,4 @@ public class Crawler {
             this.author = author;
         }
     }
-
-    // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
 }
