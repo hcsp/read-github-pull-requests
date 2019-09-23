179f00a9654bfea37775d4dd0cd65ab477a9355b
shenwl <shenweilun96@gmail.com>
2019-09-23 21:57:24 +0800

    pr爬虫 (#38)
    pr爬虫 (#38)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,12 @@
 package com.github.hcsp.http;
 
+import net.dongliu.requests.Requests;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -18,6 +25,36 @@ public class Crawler {
         }
     }
 
+    static int getNumberOfOpenInfo(String openedBy) {
+        String numberStr = openedBy.split("opened")[0];
+        return Integer.valueOf(numberStr.split("#")[1].trim());
+    }
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {
+        String baseUrl = "https://github.com/";
+        String url = baseUrl + repo + "/pulls";
+
+        String htmlString = Requests.get(url).send().readToText();
+
+        Document doc = Jsoup.parse(htmlString);
+        Elements issueElements = doc.select(".js-issue-row");
+
+        ArrayList<GitHubPullRequest> pullRequestList = new ArrayList<>();
+
+        for (Element issue : issueElements) {
+            String title = issue.select("a[data-hovercard-type=\"pull_request\"]").text();
+            String author = issue.select("a[data-hovercard-type=\"user\"]").text();
+
+            int number = getNumberOfOpenInfo(issue.select(".opened-by").text());
+
+            pullRequestList.add(new GitHubPullRequest(number, title, author));
+        }
+
+        return pullRequestList;
+    }
+
+    public static void main(String[] args) {
+        getFirstPageOfPullRequests("gradle/gradle");
+    }
 }
