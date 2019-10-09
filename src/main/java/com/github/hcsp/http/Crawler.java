d785007f55b970934b6c08f1df01a112216a8e74
903875816 <604150318@qq.com>
2019-10-09 22:45:42 +0800

    爬虫

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,21 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +35,33 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    //编号；标题，作者
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> pullRequest = new LinkedList<>();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity1 = response1.getEntity();
+            InputStream content = entity1.getContent();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            String text = IOUtils.toString(content);
+            Document document = Jsoup.parse(text);
+            ArrayList<Element> issue = new Elements(document).select("div[class=\"float-left col-8 lh-condensed p-2\"]");
+            for (Element element : issue) {
+                String pullTitle = (element.select("a[data-hovercard-type=\"pull_request\"]").text());//题目
+                int pullNumber = Integer.parseInt(element.select("span[class=\"opened-by\"]").text().substring(1, 6));//编号
+                String pullName = element.select("span[class=\"opened-by\"]").select("a").text();//名字
+                pullRequest.add(new GitHubPullRequest(pullNumber, pullTitle, pullName));
+            }
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+        System.out.println(pullRequest);
+        return pullRequest;
+    }
 }
+
+
