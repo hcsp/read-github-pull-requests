5480cb1b7f5c7ee8bf86bfe302f996226ba9ad86
Sean654343159 <654343159@qq.com>
2019-10-12 22:01:17 +0800

    debug

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,12 @@
 package com.github.hcsp.http;
 
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +26,19 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        GitHub gitHub = GitHub.connectAnonymously();
+        GHRepository repository = gitHub.getRepository(repo);
+        List<GHPullRequest> pullRequests = repository.getPullRequests(GHIssueState.OPEN);
+        List<GitHubPullRequest> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            GHPullRequest ghPullRequest = pullRequests.get(i);
+            int number = ghPullRequest.getNumber();
+            String title = ghPullRequest.getTitle();
+            String author = ghPullRequest.getUser().getLogin();
+            GitHubPullRequest gitHubPullRequest = new GitHubPullRequest(number, title, author);
+            list.add(gitHubPullRequest);
+        }
+        return list;
+    }
 }
