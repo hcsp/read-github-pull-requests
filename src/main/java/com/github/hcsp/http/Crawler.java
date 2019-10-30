5c3832fd1c19535acfd2d8385af9efb11b47a7bd
PingAn_NetBar <840495823@qq.com>
2019-10-30 18:41:09 +0800

    先用旧办法实现一遍，周末利用api优化代码。没有api得到的页面第一次爬数据真的好痛苦，大部分时间花在了找规律上面,。。。。 (#50)
    先用旧办法实现一遍，周末利用api优化代码。没有api得到的页面第一次爬数据真的好痛苦，大部分时间花在了找规律上面,。。。。 (#50)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,6 +1,20 @@
 package com.github.hcsp.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -18,6 +32,43 @@ public class Crawler {
         }
     }
 
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException{
+
+        List<GitHubPullRequest> dataOfPR = new ArrayList<GitHubPullRequest>(20);
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/"+repo+"/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            //System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            InputStream is = entity1.getContent();
+            Scanner scanner = new Scanner(is, "UTF-8");
+            String text = scanner.useDelimiter("\\A").next();
+
+            Document doc = Jsoup.parse(text);
+            Elements issues = doc.select(".js-issue-row");
+            for (Element element: issues) {
+                int number = Integer.parseInt(element.attr("id").substring(6));
+                String title = element.select(".js-navigation-open").get(0).text();
+                String author = element.select(".muted-link").get(0).text();
+                dataOfPR.add(new GitHubPullRequest( number, title, author));
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            response1.close();
+            System.out.println(dataOfPR.get(0));
+            return dataOfPR;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        getFirstPageOfPullRequests("hexojs/hexo");
+    }
 }
