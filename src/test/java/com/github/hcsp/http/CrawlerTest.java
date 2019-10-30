2b69a949d2aa2d99413a007bd4ad9a551c998767
wangyujie1207 <82414199@163.com>
2019-10-30 10:11:44 +0800

    test

--- src/test/java/com/github/hcsp/http/CrawlerTest.java
+++ src/test/java/com/github/hcsp/http/CrawlerTest.java
@@ -7,15 +7,15 @@ import org.kohsuke.github.GHPullRequest;
 import org.kohsuke.github.GitHub;
 
 public class CrawlerTest {
-//    @Test
-//    public void test() throws IOException {
-//        String repo = "gradle/gradle";
-//        Crawler.GitHubPullRequest firstPull = Crawler.getFirstPageOfPullRequests(repo).get(0);
-//
-//        GHPullRequest pull =
-//                GitHub.connectAnonymously().getRepository(repo).getPullRequest(firstPull.number);
-//
-//        Assertions.assertEquals(pull.getTitle(), firstPull.title);
-//        Assertions.assertEquals(pull.getUser().getLogin(), firstPull.author);
-//    }
+    @Test
+    public void test() throws IOException {
+        String repo = "gradle/gradle";
+        Crawler.GitHubPullRequest firstPull = Crawler.getFirstPageOfPullRequests(repo).get(0);
+
+        GHPullRequest pull =
+                GitHub.connectAnonymously().getRepository(repo).getPullRequest(firstPull.number);
+
+        Assertions.assertEquals(pull.getTitle(), firstPull.title);
+        Assertions.assertEquals(pull.getUser().getLogin(), firstPull.author);
+    }
 }
