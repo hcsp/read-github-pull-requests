ac12699cc6aff8a8877766b142a4c0d378beb386
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-11-30 22:36:12 +0800

    read-github-pull-requests,调用api获取并解析json (#61)
    read-github-pull-requests,调用api获取并解析json (#61)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,17 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -16,8 +28,53 @@ public class Crawler {
             this.title = title;
             this.author = author;
         }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+        public String getAuthor() {
+            return this.author;
+        }
+
+        public int getNumber() {
+            return this.number;
+        }
+
+        @Override
+        public String toString() {
+            return getNumber() + " ### " + getTitle() + " ### " + getAuthor();
+        }
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+
+        List<GitHubPullRequest> pullRequests = new ArrayList<>();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls?state=all&page=1");
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            String author;
+            int number;
+            String title;
+            String str = EntityUtils.toString(entity);
+            JSONArray jsonArray = JSONArray.parseArray(str);
+            for (Object object : jsonArray) {
+                JSONObject jsonObject = (JSONObject) object;
+                title = jsonObject.getString("title");
+                number = jsonObject.getInteger("number");
+                author = jsonObject.getJSONObject("user").getString("login");
+                pullRequests.add(new GitHubPullRequest(number, title, author));
+            }
+            return pullRequests;
+        } finally {
+            httpClient.close();
+            response.close();
+        }
+    }
 }
