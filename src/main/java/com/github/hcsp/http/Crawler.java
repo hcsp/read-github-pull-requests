b2ab2f3f3c9b4cf77d651449b20995ce7db8df17
benectic <xuanzebin@126.com>
2019-12-04 17:35:01 +0800

    feat: read github pull requests

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,17 @@
 package com.github.hcsp.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +31,28 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        ArrayList<GitHubPullRequest> resultList = new ArrayList<>();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls?state=all&page=1");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity1 = response1.getEntity();
+            String content = EntityUtils.toString(entity1, "UTF-8");
+            content = "{ \"content\" :" + content + '}';
+            JSONObject jsonContent = new JSONObject(content);
+            JSONArray result = (JSONArray) jsonContent.get("content");
+            for (int i = 0; i < ((JSONArray) jsonContent.get("content")).length(); i++) {
+                JSONObject item = (JSONObject) result.get(i);
+                String title = (String) item.get("title");
+                int number = (int) item.get("number");
+                JSONObject user = (JSONObject) item.get("user");
+                String userName = (String) user.get("login");
+                resultList.add(new GitHubPullRequest(number, title, userName));
+            }
+        } finally {
+            response1.close();
+        }
+        return resultList;
+    }
 }
