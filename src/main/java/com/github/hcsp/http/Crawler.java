24a79dd0924760b0d9c85ce3b7423e4ce16eeabe
guidetheorient <chrisji0518@outlook.com>
2019-12-09 15:39:34 +0800

    获取指定仓库中的Pull request信息

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,17 @@
 package com.github.hcsp.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +31,27 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+        List<GitHubPullRequest> list = new ArrayList<>();
+
+        try {
+            HttpEntity entity1 = response.getEntity();
+            Elements issueEles = Jsoup.parse(EntityUtils.toString(entity1), "utf-8").getElementsByClass("js-issue-row");
+
+            for (Element issueEle : issueEles) {
+                int number = Integer.parseInt(issueEle.attr("id").split("_")[1]);
+                String title = issueEle.getElementById(issueEle.attr("id") + "_link").text();
+                String author = issueEle.getElementsByClass("opened-by").get(0).getElementsByTag("a").get(0).text();
+                list.add(new GitHubPullRequest(number, title, author));
+            }
+        } finally {
+            response.close();
+        }
+
+        return list;
+    }
 }
