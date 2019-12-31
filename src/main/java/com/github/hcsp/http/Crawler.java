a56ce33167b5fc852e3c78d7475d3ba1e85de88a
Xi Pu <xp19@duke.edu>
2019-12-31 11:15:42 -0800

    use GitHub api to get information about pull requests

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,18 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +32,47 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo){
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(String.format("https://api.github.com/repos/%s/pulls?state=open&page=1", repo));
+        CloseableHttpResponse response1 = null;
+        try {
+            response1 = httpclient.execute(httpGet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<GitHubPullRequest> result = new ArrayList<>();
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            InputStream is = entity1.getContent();
+            String s = IOUtils.toString(is, "UTF-8");
+            JSONArray arr = JSON.parseArray(s);
+
+            for(int i = 0; i < arr.size(); i++){
+                JSONObject obj = arr.getJSONObject(i);
+                String author = obj.getJSONObject("user").getString("login");
+                String title = obj.getString("title");
+                int number = obj.getInteger("number");
+                result.add(new GitHubPullRequest(number, title, author));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response1.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        getFirstPageOfPullRequests("gradle/gradle");
+    }
 }
