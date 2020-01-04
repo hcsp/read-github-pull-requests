e3b13868112c66a002db905d1dca1e1823e999a6
Tan DuZhi <lucky.tanduzhi@gmail.com>
2020-01-04 21:33:49 +0800

    Java Bean 的使用

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,12 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -18,6 +25,62 @@ public class Crawler {
         }
     }
 
+    static class GitHubPullResponse {
+        int number;
+        String title;
+        User user;
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+        static class User {
+            String login;
+
+            public String getLogin() {
+                return login;
+            }
+
+            public void setLogin(String login) {
+                this.login = login;
+            }
+        }
+    }
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> result = new ArrayList<>();
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/repos/" + repo + "/pulls")
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            List<GitHubPullResponse> gitHubPullResponses = JSON.parseArray(response.body().string(), GitHubPullResponse.class);
+            for (GitHubPullResponse item :
+                    gitHubPullResponses) {
+                result.add(new GitHubPullRequest(item.getNumber(), item.getTitle(), item.getUser().getLogin()));
+            }
+        }
+        return result;
+    }
 }
