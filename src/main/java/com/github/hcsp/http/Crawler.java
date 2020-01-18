0870e4c93c4ee064e8e5f92b25e0870b7b72a0f4
richard1230 <huyongjinnuaa@163.com>
2020-01-18 17:35:07 +0800

    后面再用api爬取吧,先用上课讲的方法==

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -2,16 +2,7 @@ package com.github.hcsp.http;
 
 import java.util.*;
 import java.io.IOException;
-import java.util.stream.Collectors;
 
-import org.apache.http.HttpEntity;
-import org.apache.commons.io.IOUtils;
-
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import com.alibaba.fastjson.JSON;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -38,7 +29,6 @@ public class Crawler {
             this.author = author;
         }
     }
-
 
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
