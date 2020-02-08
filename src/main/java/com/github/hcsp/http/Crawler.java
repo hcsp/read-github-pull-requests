f24bb5123dd1f76e107f196b316b210dbd607f3d
silvia-YQY <545067444@qq.com>
2020-02-08 21:59:33 +0800

    checkStyle

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,21 +1,15 @@
 package com.github.hcsp.http;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.impl.client.HttpClients;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,6 +48,16 @@ public class Crawler {
     }
 
     public static void main(String[] args) throws IOException {
-        getFirstPageOfPullRequests("gradle/gradle");
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/gradle/gradle/pulls?page=1&per_page=5");
+        CloseableHttpResponse res = httpClient.execute(httpGet);
+        try{
+            HttpEntity entity = res.getEntity();
+            InputStream is = entity.getContent();
+            System.out.println(is);
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
+
