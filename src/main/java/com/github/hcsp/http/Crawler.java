3f710a59f11fcf771c6e46f930a7a098db9db415
Sunpeng526 <215860865@qq.com>
2020-02-19 17:43:51 -0800

    使用Java代码发送HTTP请求

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -34,7 +34,7 @@ public class Crawler {
         }
     }
 
-    // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
+    // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息。
     public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {
         List<GitHubPullRequest> list = new ArrayList<>();
         CloseableHttpClient httpClient = null;
