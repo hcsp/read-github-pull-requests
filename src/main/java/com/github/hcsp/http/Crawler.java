e29ff3b6088ba7bdb26ba2f375ad9688889100b6
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2020-02-19 17:47:44 -0800

    使用Java代码发送HTTP请求 (#97)
    使用Java代码发送HTTP请求 (#97)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,6 +1,22 @@
 package com.github.hcsp.http;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -18,6 +34,49 @@ public class Crawler {
         }
     }
 
-    // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息。
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {
+        List<GitHubPullRequest> list = new ArrayList<>();
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        String result = null;
+        try {
+            httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+            RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
+            httpGet.setConfig(defaultConfig);
+            response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != response) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != httpClient) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Document doc = Jsoup.parse(result);
+        Elements el = doc.select(".js-issue-row");
+        for (Element issueItem : el) {
+            String title = issueItem.select(".js-navigation-open").get(0).text();
+            String[] strings = issueItem.select(".opened-by").get(0).text().split(" ");
+            String name = strings[strings.length - 1];
+            int id = Integer.valueOf(strings[0].substring(1));
+            list.add(new GitHubPullRequest(id, title, name));
+        }
+        return list;
+    }
 }
