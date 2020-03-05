f799b36fce21b14849da5070a1bc244920dad1ab
硬核机器人 <49554844+hcsp-bot@users.noreply.github.com>
2020-03-05 23:56:41 +0800

    gradle pull request爬取 (#103)
    gradle pull request爬取 (#103)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.http;
 
+import org.eclipse.egit.github.core.PullRequest;
+import org.eclipse.egit.github.core.service.PullRequestService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -18,6 +24,24 @@ public class Crawler {
         }
     }
 
+    public static void main(String[] args) throws IOException {
+        getFirstPageOfPullRequests("gradle/gradle");
+    }
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        String[] url = repo.split("/");
+        RepositoryService service = new RepositoryService();
+        PullRequestService pullRequestService = new PullRequestService();
+        List<GitHubPullRequest> list = new ArrayList<>();
+        List<PullRequest> pullRequests = pullRequestService.getPullRequests(service.getRepository(url[0], url[1]), "ALL");
+        PullRequest pr = null;
+        int i = 0;
+        while (i < 10) {
+            pr = pullRequests.get(i);
+            list.add(new GitHubPullRequest(pr.getNumber(), pr.getTitle(), pr.getUser().getLogin()));
+            i++;
+        }
+        return list;
+    }
 }
