5ac31295c4007e22ccf7d234df445d3220ae5aaf
lolosun <lolosunrng@gmail.com>
2020-03-15 12:23:16 +0800

    使用Jsoup完成对网页的爬取

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,13 @@
 package com.github.hcsp.http;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +27,20 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        ArrayList<GitHubPullRequest> result = new ArrayList<>();
+        Document document = Jsoup.connect("https://github.com/" + repo + "/pulls").get();
+        Elements pullsElements = document.select(".js-issue-row");
+        for (Element pull : pullsElements
+        ) {
+            Element title = (Element) pull.childNode(1).childNode(5).childNode(1);
+            Element authorInfo = (Element) pull.childNode(1).childNode(5).childNode(7).childNode(1);
+            String number = authorInfo.select(".opened-by").first().text().split(" opened")[0].substring(1);
+            String author = authorInfo.select(".muted-link").text();
+            result.add(new GitHubPullRequest(Integer.parseInt(number), title.text(), author));
+        }
+        return result;
+    }
+
+
 }
