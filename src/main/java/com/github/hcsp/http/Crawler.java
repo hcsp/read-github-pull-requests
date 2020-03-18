17525504f12ac3729efffaf66a23fdd561d21aa3
JinbingLin <59944908+352503538@users.noreply.github.com>
2020-03-17 21:53:25 -0700

    HTTP实战: 获取指定仓库中的Pull request信息 (#114)
    HTTP实战: 获取指定仓库中的Pull request信息 (#114)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,18 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +32,35 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        List<GitHubPullRequest> list = new ArrayList<>();
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            InputStream is = entity1.getContent();
+            String body = IOUtils.toString(is, "UTF-8");
+            //System.out.println(body);
+            JSONArray jsonArray = JSON.parseArray(body);
+            for (Object element : jsonArray) {
+                JSONObject jsObject = (JSONObject) element;
+                int number = jsObject.getIntValue("number");
+                String title = jsObject.getString("title");
+                JSONObject userObject = (JSONObject) jsObject.get("user");
+                String author = userObject.getString("login");
+                //System.out.println(number+" "+title+" "+author);
+                list.add(new GitHubPullRequest(number, title, author));
+            }
+
+        } finally {
+            response1.close();
+        }
+        return list;
+    }
+
+//    public static void main(String[] args) throws IOException {
+//        getFirstPageOfPullRequests("gradle/gradle");
+//    }
 }
