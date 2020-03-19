6e1a51eb945f6f5e1e9e821ca98d429a0a93dcc5
tprox <61723644+tprox@users.noreply.github.com>
2020-03-20 00:20:33 +0800

    Implement getFirstPageOfPullRequests method (#120)
    Implement getFirstPageOfPullRequests method (#120)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,6 +1,20 @@
 package com.github.hcsp.http;
 
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -16,8 +30,35 @@ public class Crawler {
             this.title = title;
             this.author = author;
         }
+
+        @Override
+        public String toString() {
+            return String.format("PullRequest[%d] [%s] by [%s]", number, title, author);
+        }
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> result = new ArrayList<>();
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(String.format("https://api.github.com/repos/%s/pulls", repo));
+        httpGet.setHeader("Accept", "application/json");
+        httpGet.addHeader("Content-Type", "application/json");
+        CloseableHttpResponse response = client.execute(httpGet);
+
+        JsonNode json = new ObjectMapper().readTree(EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8));
+        Iterator<JsonNode> iterator = json.elements();
+        while (iterator.hasNext()) {
+            JsonNode pull = iterator.next();
+            result.add(new GitHubPullRequest(
+                    pull.get("number").asInt(),
+                    pull.get("title").asText(),
+                    pull.get("user").get("login").asText())
+            );
+        }
+
+        client.close();
+        return result;
+    }
 }
