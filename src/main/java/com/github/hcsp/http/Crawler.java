82225d4553296f04a1893680fb27b66eb4336fe1
Nick <545310317@qq.com>
2020-03-23 11:34:12 +0800

    使用Java代码发送HTTP请求

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,6 +1,22 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.List;
+import java.util.Map;
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -19,5 +35,29 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> resultL = new ArrayList<>();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls?page=1");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+        try {
+
+            HttpEntity entity = response.getEntity();
+            String jsonStr = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
+            List<Map<String, Object>> mapList = JSON.parseObject(jsonStr, new TypeReference<List<Map<String, Object>>>() {});
+            for (Map<String, Object> map : mapList) {
+                JSONObject userMap = (JSONObject) map.get("user");
+                resultL.add(new GitHubPullRequest(Integer.parseInt(map.get("number").toString()), map.get("title").toString(), userMap.get("login").toString()));
+            }
+
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return resultL;
+    }
 }
+
+
