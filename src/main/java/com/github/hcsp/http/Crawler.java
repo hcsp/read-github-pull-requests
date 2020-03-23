863c49985f7b4db8f3ae1d577d6613dccb8cccc5
Nick <545310317@qq.com>
2020-03-23 11:46:25 +0800

    使用Java代码发送HTTP请求

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,22 +1,19 @@
 package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.List;
-import java.util.Map;
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -36,28 +33,33 @@ public class Crawler {
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
     public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
-        List<GitHubPullRequest> resultL = new ArrayList<>();
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls?page=1");
-        CloseableHttpResponse response = httpclient.execute(httpGet);
-
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        List<GitHubPullRequest> list = new ArrayList<>();
         try {
-
-            HttpEntity entity = response.getEntity();
-            String jsonStr = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
-            List<Map<String, Object>> mapList = JSON.parseObject(jsonStr, new TypeReference<List<Map<String, Object>>>() {});
-            for (Map<String, Object> map : mapList) {
-                JSONObject userMap = (JSONObject) map.get("user");
-                resultL.add(new GitHubPullRequest(Integer.parseInt(map.get("number").toString()), map.get("title").toString(), userMap.get("login").toString()));
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            InputStream is = entity1.getContent();
+            String body = IOUtils.toString(is, "UTF-8");
+            //System.out.println(body);
+            JSONArray jsonArray = JSON.parseArray(body);
+            for (Object element : jsonArray) {
+                JSONObject jsObject = (JSONObject) element;
+                int number = jsObject.getIntValue("number");
+                String title = jsObject.getString("title");
+                JSONObject userObject = (JSONObject) jsObject.get("user");
+                String author = userObject.getString("login");
+                //System.out.println(number+" "+title+" "+author);
+                list.add(new GitHubPullRequest(number, title, author));
             }
 
-            EntityUtils.consume(entity);
         } finally {
-            response.close();
+            response1.close();
         }
-
-        return resultL;
+        return list;
     }
-}
+
+
 
 
