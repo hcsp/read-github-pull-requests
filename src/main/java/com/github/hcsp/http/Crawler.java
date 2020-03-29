725beb58a6786aef40be1e5bc7963acea5f73014
Ditto0 <46198155+Ditto0@users.noreply.github.com>
2020-03-29 18:57:24 +0800

    1

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,18 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -18,6 +31,35 @@ public class Crawler {
         }
     }
 
-    // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    //     给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/gradle/gradle/pulls");
+        httpGet.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36 Edg/80.0.361.69");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        try {
+            //System.out.println(response.getStatusLine());
+            HttpEntity entity1 = response.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            //EntityUtils.consume(entity1);
+            InputStream is = entity1.getContent();
+            String html = IOUtils.toString(is,"UTF-8");
+            Document doc = Jsoup.parse(html);
+            ArrayList<Element> pr = doc.select(".Box-row--focus-gray");
+            ArrayList<GitHubPullRequest> list = new ArrayList<>();
+            for(Element element:pr){
+                String title = element.select(".lh-condensed").select(".link-gray-dark").eachText().toString();   //title
+                int num =Integer.parseInt(element.select(".lh-condensed").select(".opened-by").get(0).childNode(0).toString().split(" ")[1].split("#")[1]);
+                String author = element.select(".lh-condensed").select(".opened-by").select(".muted-link").eachText().toString(); //by
+                list.add(new GitHubPullRequest(num,title,author));
+//                System.out.println(num);
+//                System.out.println(Integer.parseInt(element.select(".lh-condensed").select(".opened-by").get(0).childNode(0).toString().split(" ")[1].split("#")[1]));
+
+            }
+            return list;
+        } finally {
+            response.close();
+        }
+    }
 }
