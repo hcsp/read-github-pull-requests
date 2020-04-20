25f0dd1e5a04926f07e46215a69f831802bb4bc4
tfuduo <40056954+tfuduo@users.noreply.github.com>
2020-04-20 19:31:13 +0800

    Finish Crawler. (#133)
    Finish Crawler. (#133)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,17 @@
 package com.github.hcsp.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +31,33 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        //https://github.com/grdle/gradle/pulls
+        String git = "https://github.com/";
+        String url = git + repo + "/pulls";
+
+        HttpGet get = new HttpGet(url);
+        HttpEntity entity = httpClient.execute(get).getEntity();
+        //伪装浏览器
+        get.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36");
+        String html = EntityUtils.toString(entity);
+
+        Document doc = Jsoup.parse(html);
+        Elements issues = doc.getElementsByClass("Box-row Box-row--focus-gray p-0 mt-0 js-navigation-item js-issue-row");
+
+        List<GitHubPullRequest> list = new LinkedList<>();
+        for (Element issue : issues) {
+            String block = issue.getElementsByClass("Box-row Box-row--focus-gray p-0 mt-0 js-navigation-item js-issue-row").attr("id");
+            int number = Integer.parseInt(block.substring(block.indexOf("_") + 1, block.length()));
+
+            String title = issue.getElementsByAttributeValue("data-hovercard-type", "pull_request").text();
+
+            String author = issue.getElementsByAttributeValue("data-hovercard-type", "user").text();
+
+            list.add(new GitHubPullRequest(number, title, author));
+        }
+        return list;
+    }
 }
