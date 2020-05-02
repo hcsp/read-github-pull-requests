76878045db86271d192862c2250f78b2ed3c3553
EnochQin <qyqdream@gmail.com>
2020-05-02 15:03:22 +0800

    删除多余的引入

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,14 +1,12 @@
 package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.kohsuke.github.GHObject;
 import org.kohsuke.github.GHPullRequest;
 
 import java.io.IOException;
