8b16b93a8e6f8f2aeaa24dcdc75061bbeaa6a0d7
DobbyKim <1175088275@qq.com>
2020-05-20 22:45:07 +0800

    add code

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,20 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +34,36 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+
+        List<GitHubPullRequest> res = new ArrayList<>();
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/gradle/gradle/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            InputStream is = entity1.getContent();
+            String html = IOUtils.toString(is,"UTF-8");
+
+            // jsoup
+            Document document = Jsoup.parse(html);
+            ArrayList<Element> issues =  document.select(".flex-auto min-width-0 lh-condensed p-2 pr-3 pr-md-2");
+            for(Element element : issues){
+                int number = Integer.valueOf(element.child(4).child(0).text().substring(1,6));
+                String title = element.child(0).text();
+                String author = element.child(4).child(0).child(1).text();
+                res.add(new GitHubPullRequest(number,title,author));
+            }
+
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+
+        return res;
+    }
 }
