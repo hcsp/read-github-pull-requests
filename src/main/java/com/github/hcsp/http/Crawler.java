658aacb8e7e3639fd0901b1696cb473661f343d0
DobbyKimmy <48434101+DobbyKimmy@users.noreply.github.com>
2020-05-20 23:11:56 +0800

    add code (#143)
    add code (#143)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,19 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +33,36 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+
+        List<GitHubPullRequest> res = new ArrayList<>();
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            InputStream is = entity1.getContent();
+            String html = IOUtils.toString(is, "UTF-8");
+
+            // jsoup
+            Document document = Jsoup.parse(html);
+            ArrayList<Element> issues = document.select(".js-issue-row");
+            for (Element element : issues) {
+                int number = Integer.valueOf(element.id().split("_")[1]);
+                String title = element.child(0).child(1).child(0).text();
+                String author = element.child(0).select(".mt-1").select(".muted-link").text().split(" ")[0];
+                res.add(new GitHubPullRequest(number, title, author));
+            }
+
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+
+        return res;
+    }
 }
