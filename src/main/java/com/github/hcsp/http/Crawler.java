7a0e1a5946e288863701318ed95565285cecad50
wong <63686228+laowang2020@users.noreply.github.com>
2020-05-26 17:23:05 +0800

    获取Pull Request指定信息 (#144)
    获取Pull Request指定信息 (#144)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,20 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +34,38 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> list = new ArrayList<>();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+        try {
+            HttpEntity entity = response.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            InputStream is = entity.getContent();
+            String html = IOUtils.toString(is, "UTF-8");
+
+            Document doc = Jsoup.parse(html);
+            Elements issues = doc.select(".js-issue-row");
+
+            for (Element element : issues) {
+                String title = element.select(".js-navigation-open").get(0).text();
+                String[] strings = element.select(".opened-by").get(0).text().split(" ");
+                String name = strings[strings.length - 1];
+                int id = Integer.valueOf(strings[0].substring(1));
+                list.add(new GitHubPullRequest(id, title, name));
+            }
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+    }
 }
