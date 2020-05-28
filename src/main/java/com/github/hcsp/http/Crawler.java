eb721c3053448140911753a97d4465b4503de0f6
kongmu373 <62830983+kongmu373@users.noreply.github.com>
2020-05-28 13:21:58 +0800

    阅读GitHub的API文档，调用API获取并解析JSON (#152)
    阅读GitHub的API文档，调用API获取并解析JSON (#152)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,19 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +33,27 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        List<GitHubPullRequest> gitHubPullRequests = new ArrayList<>();
+        try {
+            HttpEntity entity1 = response1.getEntity();
+            InputStream content = entity1.getContent();
+
+            String html = IOUtils.toString(content, StandardCharsets.UTF_8);
+            JSONArray objects = JSON.parseArray(html);
+            for (Object object : objects) {
+                String title = (String) ((JSONObject) object).get("title");
+                Object user = ((JSONObject) object).get("user");
+                String author = (String) ((JSONObject) user).get("login");
+                int number = (int) (((JSONObject) object).get("number"));
+                gitHubPullRequests.add(new GitHubPullRequest(number, title, author));
+            }
+        } finally {
+            response1.close();
+        }
+        return gitHubPullRequests;
+    }
 }
