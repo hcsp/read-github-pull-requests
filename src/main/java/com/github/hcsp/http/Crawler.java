ac9574975362395e0b190ec8d748b279ff39edb5
JonesWilde <davidgluttongk@outlook.com>
2020-06-05 23:33:08 -0700

    使用暴力方法获取 根据jsoup文档, jsoup提供获取html并转换成document，不需要使用 httpclient 和 commons-io 依赖做冗余操作转换

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.http;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +25,21 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> res = new ArrayList<>();
+
+        String url = "https://github.com/"+repo+"/pulls";
+        Document doc = Jsoup.connect(url).get();
+
+        ArrayList<Element> issue = doc.select(".js-issue-row");
+        for (Element element : issue) {
+            int number = Integer.parseInt(element.select(".mt-1.text-small.text-gray").get(0).child(0).text().split(" ")[0].substring(1));
+            String title = element.child(0).child(1).child(0).text();
+            String author = element.select(".mt-1.text-small.text-gray").get(0).child(0).child(1).text();
+            GitHubPullRequest tmp = new GitHubPullRequest(number, title, author);
+            res.add(tmp);
+        }
+
+        return res;
+    }
 }
