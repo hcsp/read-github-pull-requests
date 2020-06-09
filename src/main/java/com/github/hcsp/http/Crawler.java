7780a05f4bdcad5850c790dfd0df3f5e7e5de21a
lazzben <458861463@qq.com>
2020-06-09 20:24:03 +0800

    获取指定仓库中的Pull request信息

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,19 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -16,8 +30,40 @@ public class Crawler {
             this.title = title;
             this.author = author;
         }
+
+        @Override
+        public String toString() {
+            return "GitHubPullRequest{" +
+                    "number=" + number +
+                    ", title='" + title + '\'' +
+                    ", author='" + author + '\'' +
+                    '}';
+        }
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> result = new ArrayList<>();
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/issues");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+
+        try {
+            HttpEntity entity1 = response1.getEntity();
+            InputStream is = entity1.getContent();
+            String html = IOUtils.toString(is, StandardCharsets.UTF_8);
+            Document document = Jsoup.parse(html);
+            ArrayList<Element> issues = document.select(".js-issue-row");
+            for (Element issue : issues) {
+                String number = issue.attr("id").replaceAll("\\D", "");
+                String title = issue.select("[data-hovercard-type=issue]").text();
+                String author = issue.select("[data-hovercard-type=user]").text();
+                result.add(new GitHubPullRequest(Integer.parseInt(number), title, author));
+            }
+        } finally {
+            response1.close();
+        }
+        return result;
+    }
 }
