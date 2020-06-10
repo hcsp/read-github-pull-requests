1bd80dc55359e163540da0376539704a8c3dadee
Haixiang <haixiang6123@gmail.com>
2020-06-10 14:07:04 +0800

    获取指定仓库中的Pull request信息

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.http;
 
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +25,18 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> results = new ArrayList<>();
+        List<GHPullRequest> pullRequests = GitHub.connectAnonymously().getRepository(repo).getPullRequests(GHIssueState.OPEN);
+
+        for (GHPullRequest pullRequest : pullRequests) {
+            results.add(new GitHubPullRequest(
+                    pullRequest.getNumber(),
+                    pullRequest.getTitle(),
+                    pullRequest.getUser().getLogin()
+            ));
+        }
+
+        return results;
+    }
 }
