f04c1fc64189be7f24b945cfa90da08de44c6aae
Leo <790868516@qq.com>
2020-07-28 02:17:00 +0800

    HTTP实战: 获取指定仓库中的Pull request信息

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,4 @@
 package com.github.hcsp.http;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -8,14 +7,13 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -38,23 +36,25 @@ public class Crawler {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
         CloseableHttpResponse response = httpclient.execute(httpGet);
-        List<GitHubPullRequest> myList = new ArrayList<>();
+        List<GitHubPullRequest> result = new ArrayList<>();
         try {
             HttpEntity entity = response.getEntity();
             InputStream is = entity.getContent();
             String Html = IOUtils.toString(is, StandardCharsets.UTF_8);
             Document doc = Jsoup.parse(Html);
             Elements issues = doc.select(".js-issue-row");
-            for (int i = 0; i < issues.size(); i++) {
-                String title = issues.get(i).child(0).child(1).child(0).text();
-                String author = issues.get(i).child(0).child(1).select(".mt-1").get(0).child(0).select("a").text();
-                GitHubPullRequest element = new GitHubPullRequest(i + 1, title, author);
-                myList.add(element);
+            for (Element element : issues) {
+                String title = element.child(0).child(1).child(0).text();
+                String author = element.child(0).child(1).select(".mt-1").get(0).child(0).select("a").text();
+                String obtainNum = element.child(0).child(1).child(0).attr("href");
+                int newNum = Integer.parseInt(obtainNum.replace("/" + repo + "/pull/", ""));
+                GitHubPullRequest gitHubPullRequest = new GitHubPullRequest(newNum, title, author);
+                result.add(gitHubPullRequest);
             }
         } finally {
             response.close();
         }
-        return myList;
+        return result;
     }
 }
 
