d36545910d60659d908e16a34a238d0dafdd5dd3
luckzhu <luckzhu1992@gmail.com>
2020-07-29 17:01:11 +0800

    read-github-pull-requests

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,6 +1,17 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -19,5 +30,28 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.github.com/repos/"+repo+"/pulls?page=1&per_page=10")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("服务器端错误: " + response);
+        }
+        String responseBodyString = Objects.requireNonNull(response.body()).string();
+        JSONArray jsonArray = JSON.parseArray(responseBodyString);
+
+        List<GitHubPullRequest> list = new ArrayList<>();
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonOne = jsonArray.getJSONObject(i);
+            int number = Integer.parseInt(jsonOne.getString("number"));
+            String title = jsonOne.getString("title");
+            String author = jsonOne.getJSONObject("user").getString("login");
+            list.add(new GitHubPullRequest(number, title, author));
+        }
+        return list;
+    }
 }
