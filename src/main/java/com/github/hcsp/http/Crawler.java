a93adda37694dc4e8eb1ef74365a072fe4d00f47
pengyj <pengya@eastpharm.com>
2020-08-19 15:12:09 +0800

    格式化代码

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -2,19 +2,13 @@ package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-
 import org.apache.http.HttpEntity;
-
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
-
 import java.io.IOException;
-
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,22 +27,23 @@ public class Crawler {
             this.author = author;
         }
     }
+
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
     public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
         List<GitHubPullRequest> list = new ArrayList<>();
         CloseableHttpClient httpClient = HttpClients.createDefault();//打开链接
-        HttpGet httpGet= new HttpGet("https://api.github.com/repos/" + repo + "/pulls");//获取请求
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");//获取请求
         CloseableHttpResponse response = httpClient.execute(httpGet);//得到数据
         HttpEntity entity = response.getEntity();//转成实体
-        String html1 = EntityUtils.toString(entity,"utf-8");//从实体转成json编码的字符串
-        JSONArray array =  JSONArray.parseArray(html1);//使用fastjson的方法转成一个json的数组
+        String html1 = EntityUtils.toString(entity, "utf-8");//从实体转成json编码的字符串
+        JSONArray array = JSONArray.parseArray(html1);//使用fastjson的方法转成一个json的数组
         for (Object object : array) {//遍历数组，得到想要的数据
             String title = (String) ((JSONObject) object).get("title");
             Object user = ((JSONObject) object).get("user");
             String author = (String) ((JSONObject) user).get("login");
             int number = (int) (((JSONObject) object).get("number"));
-            list.add(new GitHubPullRequest(number,title,author));
-            System.out.println("request的编号为："+number+"。request的标题为："+title+"。GitHub 用户名为："+author+"。");
+            list.add(new GitHubPullRequest(number, title, author));
+            System.out.println("request的编号为：" + number + "。request的标题为：" + title + "。GitHub 用户名为：" + author + "。");
         }
         return list;
     }
