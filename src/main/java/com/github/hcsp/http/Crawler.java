0e5ee7b2496a192bd909cea7ebd87022f1932a55
pengyj <961942635@qq.com>
2020-08-19 22:54:28 +0800

    使用api得到数据

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -2,13 +2,16 @@ package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,15 +38,17 @@ public class Crawler {
         HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");//获取请求
         CloseableHttpResponse response = httpClient.execute(httpGet);//得到数据
         HttpEntity entity = response.getEntity();//转成实体
-        String html1 = EntityUtils.toString(entity, "utf-8");//从实体转成json编码的字符串
-        JSONArray array = JSONArray.parseArray(html1);//使用fastjson的方法转成一个json的数组
+        InputStream inputStream = entity.getContent();//得到流的boby
+        String string = IOUtils.toString(inputStream, "utf-8");//把得到的stream转成字符串
+        //String html1 = EntityUtils.toString(entity, "utf-8");//从实体转成json编码的字符串
+        JSONArray array = JSONArray.parseArray(string);//使用fastjson的方法转成一个json的数组
         for (Object object : array) {//遍历数组，得到想要的数据
             String title = (String) ((JSONObject) object).get("title");
             Object user = ((JSONObject) object).get("user");
             String author = (String) ((JSONObject) user).get("login");
             int number = (int) (((JSONObject) object).get("number"));
             list.add(new GitHubPullRequest(number, title, author));
-           // System.out.println("request的编号为：" + number + "。request的标题为：" + title + "。GitHub 用户名为：" + author + "。");
+            // System.out.println("request的编号为：" + number + "。request的标题为：" + title + "。GitHub 用户名为：" + author + "。");
         }
         return list;
     }
