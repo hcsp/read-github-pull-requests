938c3c2d31f521b187d86d813adc198da2c1ee69
Administrator <619805205@qq.com>
2020-10-23 00:47:03 +0800

    完成爬虫

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,18 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -11,6 +24,7 @@ public class Crawler {
         // Pull request的作者的 GitHub 用户名
         String author;
 
+
         GitHubPullRequest(int number, String title, String author) {
             this.number = number;
             this.title = title;
@@ -19,5 +33,50 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        ArrayList<GitHubPullRequest> target = new ArrayList<>();
+        try {
+            HttpGet httpget = new HttpGet("https://api.github.com/repos/" + repo + "/pulls?page=1");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+
+
+            List<String> list = JSON.parseArray(responseBody, String.class);
+            for (String item : list
+            ) {
+                JSONObject obj = JSON.parseObject(item);
+                int number = (int) obj.get("number");
+                String title = obj.get("title").toString();
+                JSONObject user = JSON.parseObject(obj.get("user").toString());
+                String author = user.get("login").toString();
+                target.add(new GitHubPullRequest(number, title, author));
+            }
+            return target;
+        } finally {
+            httpclient.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getFirstPageOfPullRequests("vuejs/vue"));
+    }
 }
