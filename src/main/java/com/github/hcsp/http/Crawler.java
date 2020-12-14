185909458a79e8922c232b3d29bddfb102e3141d
weiranyi <67884344+weiranyi@users.noreply.github.com>
2020-12-14 10:38:11 +0800

    仿照老师，使用httpclient、commons-io、jsoup实现了对GitHub，golang/go的信息爬取 (#186)
    仿照老师，使用httpclient、commons-io、jsoup实现了对GitHub，golang/go的信息爬取 (#186)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,18 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +32,49 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        //创建一个对象集合
+        ArrayList<GitHubPullRequest> pullRequest = new ArrayList<>();
+
+        //1.1、创建一个客户端 【httpcomponents maven得来】
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        //1.2、设置请求方式，这里是Get请求【httpcomponents maven得来】
+        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+
+        //1.3、用httpclient客户端去执行httpGet请求。拿到response1响应
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+        try {
+            //1、设置页面请求------------------------------------------------
+            //查看响应状态是否为200
+            System.out.println(response.getStatusLine());
+            // 对一个响应体做一些有用的处理。HttpEntity是实体
+            HttpEntity entity = response.getEntity();
+            //拿到请求的数据流 commons-io
+            InputStream is = entity.getContent();
+            //1、设置页面响应------------------------------------------------
+            //设置解析编码为UTF-8
+            String html = IOUtils.toString(is, "UTF-8");
+            //通过jsoup去解析html页面为文档,System.out.println(document);
+            Document document = Jsoup.parse(html);
+            //通过select去选出Class中含有***类的元素
+            ArrayList<Element> pulls = document.select(".js-issue-row");
+
+            for (Element element : pulls) {
+                pullRequest.add(
+                        new GitHubPullRequest(
+                                //获取编号element.child(0).child(1).child(3).child(0).text()属于特定页面
+                                Integer.parseInt(element.select("span[class=\"opened-by\"]").text().substring(1, 6)),
+                                //获取标题element.child(0).child(1).child(0).text()属于特定页面
+                                element.select(".h4").text(),
+                                //获取作者element.child(0).child(1).child(3).child(0).child(1).text()属于特定页面
+                                element.select("span[class=\"opened-by\"]").select("a").text()
+                        )
+                );
+            }
+            return pullRequest;
+        } finally {
+            response.close();
+        }
+    }
 }
