5dd0510f589e9522b2d62f0e5a92b230b2f6e0cd
Ay <35792478+IveBecomeSoNumb@users.noreply.github.com>
2020-12-15 10:38:23 -0800

    第一次提交 (#187)
    第一次提交 (#187)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,10 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSONArray;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +24,20 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        int pullRequestNumber = 5;
+        ArrayList<GitHubPullRequest> list = new ArrayList<>();
+        String url = "https://api.github.com/repos/" + repo + "/pulls?page=1&per_page=5";
+        String json = Jsoup.connect(url).ignoreContentType(true).execute().body();
+        JSONArray jsonArray = JSONArray.parseArray(json);
+
+        for (int i = 0; i < pullRequestNumber; i++) {
+            int number = jsonArray.getJSONObject(i).getInteger("number");
+            String title = jsonArray.getJSONObject(i).getString("title");
+            String author = jsonArray.getJSONObject(i).getJSONObject("user").getString("login");
+            GitHubPullRequest githubpullrequest = new GitHubPullRequest(number, title, author);
+            list.add(githubpullrequest);
+        }
+        return list;
+    }
 }
