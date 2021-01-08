6eea6d92a3e8608035263ead874ae81073cd0316
XuKaiQuan <964262001@qq.com>
2021-01-08 12:28:44 +0800

    使用Java代码发送HTTP请求

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -6,6 +6,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -30,6 +31,7 @@ public class Crawler {
             this.title = title;
             this.author = author;
         }
+
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
@@ -48,18 +50,22 @@ public class Crawler {
 
             ArrayList<Element> issues = doc.select(".js-issue-row");
             for (Element element : issues) {
-                int number = Integer.parseInt(element.child(0).child(1).
-                        child(3).child(0).text().substring(1, 6));
+                int number = Integer.parseInt(element.child(0).child(1).child(3).child(0).text().substring(1, 6));
                 String title = element.child(0).child(1).child(0).text();
                 String author = element.child(0).child(1).child(3).child(0).child(1).text();
-                GitHubPullRequest temp = new GitHubPullRequest(number,title,author);
+                System.out.println(number);
+                System.out.println(title);
+                System.out.println(author);
+
+                GitHubPullRequest temp = new GitHubPullRequest(number, title, author);
                 list.add(temp);
             }
-
+            EntityUtils.consume(entity1);
         } finally {
             response1.close();
         }
         return list;
     }
+
 }
 
