7d5466ed3c1dcc61c1f372f6240e864aebd10ef9
greek-zzf <1531037123@qq.com>
2021-03-04 18:20:58 +0800

    Use the github API

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,18 @@
 package com.github.hcsp.http;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +32,37 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> gitHubPullRequests = new ArrayList<>();
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");
+        httpGet.setHeader("Accept", "application/vnd.github.v3+json");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+
+        try {
+            HttpEntity responseEntity = response.getEntity();
+            InputStream inputStream = responseEntity.getContent();
+            String result = IOUtils.toString(inputStream, "UTF-8");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode node = objectMapper.readTree(result);
+
+            int number;
+            String title;
+            String author;
+            for (int i = 0; i < node.size(); i++) {
+                number = node.get(i).get("number").asInt();
+                title = node.get(i).get("title").asText();
+                author = node.get(i).get("user").get("login").asText();
+
+                gitHubPullRequests.add(new GitHubPullRequest(number, title, author));
+            }
+            EntityUtils.consume(responseEntity);
+        } finally {
+            response.close();
+        }
+        return gitHubPullRequests;
+    }
 }
+
