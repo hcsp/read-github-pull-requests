082e945ae9a33171af3822897462c11abc0b5268
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-05-06 18:07:32 +0800

    第一次尝试使课堂方法爬虫www.github.com/gradle/gradle/pulls信息 (#197)
    第一次尝试使课堂方法爬虫www.github.com/gradle/gradle/pulls信息 (#197)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,19 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +33,30 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> list = new ArrayList<>();
+        try (
+                CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+            try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+                System.out.println(response.getStatusLine());
+                HttpEntity entity1 = response.getEntity();
+                // do something useful with the response body
+                // and ensure it is fully consumed
+                InputStream is = entity1.getContent();
+                String html = IOUtils.toString(is, StandardCharsets.UTF_8);
+                Document doc = Jsoup.parse(html);
+                ArrayList<Element> pulls = doc.select(".js-issue-row");
+
+                for (Element element : pulls) {
+                    int size = element.child(0).child(1).childrenSize();
+                    int number = Integer.parseInt(element.child(0).child(1).child(size - 1).child(0).text().substring(1, 6));
+                    String title = element.child(0).child(1).child(0).text();
+                    String author = element.child(0).child(1).child(size - 1).child(0).child(1).text();
+                    list.add(new GitHubPullRequest(number, title, author));
+                }
+            }
+        }
+        return list;
+    }
 }
