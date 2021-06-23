5e7f1f4f9e0298157ad882e200641e62d6c5f1d4
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-06-23 22:00:35 +0800

    feat: complete get repo pull request data (#201)
    feat: complete get repo pull request data (#201)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,15 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +29,102 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {
+        String url = "https://api.github.com/repos/" + repo + "/pulls";
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet(url);
+            try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+                System.out.println(response.getStatusLine());
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                StringBuilder stringBuilder = new StringBuilder();
+                while (true) {
+                    int b = content.read();
+                    if (b == -1) {
+                        break;
+                    }
+                    stringBuilder.append((char) b);
+                }
+                List<Pull> pullList = JSON.parseArray(String.valueOf(stringBuilder), Pull.class);
+                List<GitHubPullRequest> result = new ArrayList<>();
+                for (Pull pull : pullList) {
+                    GitHubPullRequest pr = new GitHubPullRequest(pull.getNumber(), pull.getTitle(), pull.getUser().getLogin());
+                    result.add(pr);
+                }
+                return result;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    private static class User {
+        private String login;
+        private int id;
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
+
+    private static class Pull {
+        private String url;
+        private int id;
+        private User user;
+        private String title;
+        private int number;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+    }
 }
