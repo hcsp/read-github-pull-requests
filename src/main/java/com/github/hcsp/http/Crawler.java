7824a89ee367be16a6180a6bd11b48bdd1aa885d
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-06-24 20:53:12 +0800

    Feature/task (#202)
    Feature/task (#202)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.http;
 
+import org.eclipse.egit.github.core.PullRequest;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.service.PullRequestService;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +25,17 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        String owner = repo.split("/")[0];
+        String name = repo.split("/")[1];
+        PullRequestService service = new PullRequestService();
+        RepositoryId repositoryId = new RepositoryId(owner, name);
+        List<PullRequest> pullRequestsList = service.getPullRequests(repositoryId, "");
+        List<GitHubPullRequest> result = new ArrayList<>();
+        for (PullRequest pullRequest : pullRequestsList) {
+            GitHubPullRequest pr = new GitHubPullRequest(pullRequest.getNumber(), pullRequest.getTitle(), pullRequest.getUser().getLogin());
+            result.add(pr);
+        }
+        return result;
+    }
 }
