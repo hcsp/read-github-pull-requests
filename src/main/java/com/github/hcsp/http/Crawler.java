298345d3be3536cea87e9c556eaed8c97e603184
Ludwig <392559142@qq.com>
2021-10-10 01:30:52 +0800

    0826 (#207)
    0826 (#207)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,10 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSONArray;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +24,21 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {
+
+        ArrayList<GitHubPullRequest> list = new ArrayList<>();
+        String url = "https://api.github.com/repos/" + repo + "/pulls?page=1&per_page=5";
+        try {
+            String json = Jsoup.connect(url).ignoreContentType(true).execute().body();
+            JSONArray jsonArray = JSONArray.parseArray(json);
+            int number = jsonArray.getJSONObject(0).getInteger("number");
+            String title = jsonArray.getJSONObject(0).getString("title");
+            String auther = jsonArray.getJSONObject(0).getJSONObject("user").getString("login");
+            GitHubPullRequest gitHubPullRequest = new GitHubPullRequest(number, title, auther);
+            list.add(gitHubPullRequest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
