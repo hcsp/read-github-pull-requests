23217e3d62092c3d84ab24ce74bea37c0824990d
饶家俊 <raojiajun@dayuwuxian.com>
2021-10-21 20:32:29 +0800

    正在完成需求

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.http;
 
 import java.util.List;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -19,5 +25,31 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        String requestUrl = "https://api.github.com/repos/" + repo + "/pulls";
+
+        HttpGet httpGet = new HttpGet(requestUrl);
+
+
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("fdsfdas");
+
+
+
+
+    }
 }
