a8c5e9bea6c303f3907047237b45dc376f72ce95
赵童 <freetie@qq.com>
2022-02-27 23:32:24 +0800

    finish getFirstPageOfPullRequests (#223)
    finish getFirstPageOfPullRequests (#223)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,9 +1,19 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Crawler {
     static class GitHubPullRequest {
+        private Map<String, Object> user;
         // Pull request的编号
         int number;
         // Pull request的标题
@@ -11,13 +21,45 @@ public class Crawler {
         // Pull request的作者的 GitHub 用户名
         String author;
 
-        GitHubPullRequest(int number, String title, String author) {
+        public Map<String, Object> getUser() {
+            return user;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
             this.number = number;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
             this.title = title;
-            this.author = author;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+        }
+
+        public void setUser(Map<String, Object> user) {
+            this.user = user;
+            this.author = (String) user.get("login");
         }
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        String jsonString = IOUtils.toString(response.getEntity().getContent());
+        return JSON.parseArray(jsonString, GitHubPullRequest.class);
+    }
 }
