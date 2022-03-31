375dc7eed291afba77a89751ff887d01ca355e52
magicalmuggle <richardhtw@outlook.com>
2022-03-31 13:59:09 +0800

    获取指定仓库中的Pull request信息 (#224)
    获取指定仓库中的Pull request信息 (#224)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,6 +1,24 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Crawler {
     static class GitHubPullRequest {
@@ -19,5 +37,39 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> gitHubPullRequestList = new ArrayList<>();
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/" + repo + "/pulls");
+        HttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        InputStream inputStream = entity.getContent();
+        String html = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Document document = Jsoup.parse(html);
+        Elements issues = document.select(".js-issue-row");
+
+        for (Element issue : issues) {
+            issue = issue.child(0).child(1);
+            int number = getNumbersFromString(issue.child(0).lastElementSibling().child(0).text().split("opened")[0]);
+            String title = issue.child(0).text();
+            String author = issue.child(0).lastElementSibling().child(0).child(1).text();
+            gitHubPullRequestList.add(new GitHubPullRequest(number, title, author));
+        }
+        EntityUtils.consume(entity);
+
+        return gitHubPullRequestList;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getFirstPageOfPullRequests("gradle/gradle"));
+    }
+
+    public static int getNumbersFromString(String content) {
+        String regEx = "[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(content);
+        String numbersString = m.replaceAll("");
+        return Integer.parseInt(numbersString);
+    }
 }
