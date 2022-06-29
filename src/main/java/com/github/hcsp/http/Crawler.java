655a242b9d8a9ca8b27217d4c3ae018354f4a21b
zj <woaiwrf@qq.com>
2022-06-29 10:47:42 +0800

    tst

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,24 @@
 package com.github.hcsp.http;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +38,38 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        List<GitHubPullRequest> list = new ArrayList<>();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://github.com/"+repo+"/pulls");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+    // The underlying HTTP connection is still held by the response object
+    // to allow the response content to be streamed directly from the network socket.
+    // In order to ensure correct deallocation of system resources
+    // the user MUST call CloseableHttpResponse#close() from a finally clause.
+    // Please note that if response content is not fully consumed the underlying
+    // connection cannot be safely re-used and will be shut down and discarded
+    // by the connection manager.
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            InputStream is = entity1.getContent();
+            String result = IOUtils.toString(is, "UTF-8");
+            Document doc = Jsoup.parse(result);
+            Elements newsHeadlines = doc.select(".js-active-navigation-container").select(".js-navigation-item");
+            for (Element element:newsHeadlines) {
+                int number = Integer.valueOf(element.child(0).child(1).child(1).child(0).text().split(" ")[0].split("#")[1]);
+                String title = element.child(0).child(1).child(0).text();
+                String autor = element.child(0).child(1).child(1).child(0).text().split(" ")[0];
+                GitHubPullRequest gitHubPullRequest = new GitHubPullRequest(number,title,autor);
+                list.add(gitHubPullRequest);
+            }
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+        return list;
+    }
 }
