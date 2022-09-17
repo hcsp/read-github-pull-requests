60277e8f70701f133a450fce4818c5d151aa9a8d
Jingxin Wang <wjxalexander@gmail.com>
2022-09-17 22:17:04 +0800

    done

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,8 +1,54 @@
 package com.github.hcsp.http;
 
+import java.net.URISyntaxException;
 import java.util.List;
+import java.io.IOException;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NoArgsConstructor;
+import org.apache.hc.client5.http.ClientProtocolException;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
 
 public class Crawler {
+    @NoArgsConstructor
+    static class User {
+        public String login;
+
+        public User(String login) {
+            this.login = login;
+        }
+    }
+
+    @NoArgsConstructor
+    static class OriginalPullRequest {
+        @JsonProperty("number")
+        int number;
+        // Pull request的标题
+        @JsonProperty("title")
+        String title;
+
+        @JsonProperty("user")
+        User user;
+
+        public OriginalPullRequest(int number, String title, User user) {
+            this.number = number;
+            this.title = title;
+            this.user = user;
+        }
+    }
+
     static class GitHubPullRequest {
         // Pull request的编号
         int number;
@@ -11,13 +57,50 @@ public class Crawler {
         // Pull request的作者的 GitHub 用户名
         String author;
 
-        GitHubPullRequest(int number, String title, String author) {
+        public GitHubPullRequest(int number, String title, String author) {
             this.number = number;
             this.title = title;
             this.author = author;
         }
+
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException {
+        try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            final HttpGet httpget = new HttpGet("https://api.github.com/repos/" + repo + "/pulls");
+            System.out.println("Executing request " + httpget.getMethod() + " " + httpget.getUri());
+            // Create a custom response handler
+            final HttpClientResponseHandler<String> responseHandler = response -> {
+                final int status = response.getCode();
+                if (status >= HttpStatus.SC_SUCCESS && status < HttpStatus.SC_REDIRECTION) {
+                    final HttpEntity entity = response.getEntity();
+                    try {
+                        if (entity == null) {
+                            return null;
+                        }
+                        return EntityUtils.toString(entity);
+                    } catch (final ParseException ex) {
+                        throw new ClientProtocolException(ex);
+                    }
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            final String responseBody = httpclient.execute(httpget, responseHandler);
+            return convertResponseBodyToList(responseBody);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static List<GitHubPullRequest> convertResponseBodyToList(String entity) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return mapper.readValue(entity, new TypeReference<List<OriginalPullRequest>>() {
+        }).stream().map(item -> new GitHubPullRequest(item.number, item.title, item.user.login)).toList();
+    }
+
 }
